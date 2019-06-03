9145b621d0b28298107f3aa35b3bf28303276220
apata <apata@incert.lu>
2019-06-03 11:11:21 +0200

    Create project 01

---
+++ 01_BasicORM/src/main/java/ro/pata/jpa/eni/App.java
@@ -0,0 +1,13 @@
+package ro.pata.jpa.eni;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
