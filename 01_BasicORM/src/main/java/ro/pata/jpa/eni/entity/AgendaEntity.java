a042379db003a639700ced8a62a0fa1b6e1f2b3d
apata <apata@incert.lu>
2019-06-07 14:03:05 +0200

    Test primary key strategy auto/identity/table/sequence

---
+++ 01_BasicORM/src/main/java/ro/pata/jpa/eni/entity/AgendaEntity.java
@@ -0,0 +1,68 @@
+package ro.pata.jpa.eni.entity;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "agenda", schema = "adi", catalog = "")
+@SequenceGenerator(name = "seqGen")
+public class AgendaEntity {
+    private int id;
+    private String nume;
+    private String telefon;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seqGen")
+    @Column(name = "id", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "nume", nullable = true, length = 30)
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    @Basic
+    @Column(name = "telefon", nullable = true, length = 30)
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AgendaEntity that = (AgendaEntity) o;
+        return id == that.id &&
+                Objects.equals(nume, that.nume) &&
+                Objects.equals(telefon, that.telefon);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nume, telefon);
+    }
+
+    @Override
+    public String toString() {
+        return "AgendaEntity{" +
+                "id=" + id +
+                ", nume='" + nume + '\'' +
+                ", telefon='" + telefon + '\'' +
+                '}';
+    }
+}
